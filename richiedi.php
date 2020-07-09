d408990ec2fd4af8ad6db933255b357d8b18a840
Progetto in Arduino: Misurazioni ambientali - OpenDay 2019 <57725680+arduinofauser2019@users.noreply.github.com>
2020-07-09 10:33:57 +0200

    Componenti di visualizzazione delle misurazioni
    Componenti di visualizzazione delle misurazioni

--- richiedi.php
+++ richiedi.php
@@ -2,7 +2,8 @@
 try {
     $misure = "";
     // Viene restituito il file richiesto, se non contiene caratteri "/" (per evitare tentativi di accesso ad altre cartelle)
-    if (file_exists($_REQUEST["nomeFile"]) && (strpos($_REQUEST["nomeFile"], '/') !== false) == false) {
+    if (((preg_match("/^[A-Za-z0-9 -]{0,}[.]txt$/", $_REQUEST["nomeFile"])) &&
+            (strpos($_REQUEST["nomeFile"], '/') === false) && file_exists($_REQUEST["nomeFile"])) == true) {
         $misureFile = fopen($_REQUEST["nomeFile"], "r");
         while (!feof($misureFile)) {
             $linea = fgets($misureFile);
